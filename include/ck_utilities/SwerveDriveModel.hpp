ebcdbf67fb4e943e673e8f170321863c1f5c427d
= <=>
2022-09-17 16:38:22 +0000

    Add swerve drive model

---
+++ include/ck_utilities/SwerveDriveModel.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+struct SwerveDriveModel
+{
+public:
+    double front_left_wheel_velocity;
+    double front_right_wheel_velocity;
+    double rear_left_wheel_velocity;
+    double rear_right_wheel_velocity;
+
+    double front_left_wheel_angle
+    double front_right_wheel_angle
+    double rear_left_wheel_angle
+    double rear_right_wheel_angle
+};
