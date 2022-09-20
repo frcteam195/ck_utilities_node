71ab6e89d507b219eb740fcff3a9f75b5ef0814f
Michael Todd <eltodd@gmail.com>
2022-09-20 23:14:32 +0000

    Fix compilation issues

--- include/ck_utilities/SwerveDriveOutput.hpp
+++ include/ck_utilities/SwerveDriveOutput.hpp
@@ -8,8 +8,8 @@ public:
     double rear_left_wheel_velocity;
     double rear_right_wheel_velocity;
 
-    double front_left_wheel_angle
-    double front_right_wheel_angle
-    double rear_left_wheel_angle
-    double rear_right_wheel_angle
+    double front_left_wheel_angle;
+    double front_right_wheel_angle;
+    double rear_left_wheel_angle;
+    double rear_right_wheel_angle;
 };
