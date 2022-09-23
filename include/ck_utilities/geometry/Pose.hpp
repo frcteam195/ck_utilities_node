0e6ee04a59c6dc6944c794e9f858a258e3059d6c
Michael Todd <eltodd@gmail.com>
2022-09-23 02:01:45 +0000

    Commit WIP

--- include/ck_utilities/geometry/Pose.hpp
+++ include/ck_utilities/geometry/Pose.hpp
@@ -1,25 +1,56 @@
+#pragma once
+
 #if __INTELLISENSE__
 #undef __ARM_NEON
 #undef __ARM_NEON__
 #endif
 
 #include "Eigen/Dense"
-
-class Pose
+namespace geometry
 {
-public:
-    Eigen::Vector3f position;
-    // Eigen::Quaternion<float> orientation;
-};
+    class Pose; class Transform; class Twist;
 
-class Transform
-{
-public:
-    Eigen::Vector3f linear;
-    // Eigen::Quaternion<float> angular;
-};
+    typedef Eigen::Vector3f Rotation;
+    typedef Eigen::Vector3f Translation;
 
-typedef Transform Twist;
+    class Pose
+    {
+    public:
+        Pose()
+        {
+            this->position.setZero();
+            this->orientation.setIdentity();
+        }
+        Pose twist(Twist twist_, double time_s);
+        Pose transform(Transform transform_);
+        Transform get_Transform(Pose pose_);
+        Translation position;
+        Rotation orientation;
+    };
 
+    class Transform
+    {
+    public:
+        Transform()
+        {
+            this->linear.setZero();
+            this->angular.setIdentity();
+        }
+        Transform rotate(Rotation rotation);
+        Translation linear;
+        Rotation angular;
+    };
 
+    class  Twist
+    {
+    public:
+        Twist()
+        {
+            this->linear.setZero();
+            this->angular.setIdentity();
+        }
+        Translation linear;
+        Rotation angular;
+    };
 
+}
