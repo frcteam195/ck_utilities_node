b42b4e91299739f093647d229dea70087a1cedd5
BonomiC <bonomi.c11@gmail.com>
2023-01-22 02:44:42 +0000

    Update wheel diameter

--- include/ck_utilities/Constants.hpp
+++ include/ck_utilities/Constants.hpp
@@ -25,7 +25,8 @@ namespace ck
     constexpr double K_LOG_REPORT_RATE = 0.02;
 
     constexpr double  K_DRIVE_WHEEL_TRACK_WIDTH_INCHES = 25.625;
-    constexpr double  K_DRIVE_WHEEL_DIAMETER_INCHES = 4.0025;
+    // constexpr double  K_DRIVE_WHEEL_DIAMETER_INCHES = 4.0025;
+    constexpr double  K_DRIVE_WHEEL_DIAMETER_INCHES = 3.9;
     // constexpr double  K_DRIVE_WHEEL_DIAMETER_INCHES = 4.00;
     constexpr double  K_DRIVE_WHEEL_RADIUS_INCHES = K_DRIVE_WHEEL_DIAMETER_INCHES / 2.0;
     constexpr double K_TRACK_SCRUB_FACTOR = 1.0; // Tune me!
@@ -46,5 +47,5 @@ namespace ck
     constexpr double kAdaptivePathMinLookaheadDistance = 6.0;
     constexpr double kAdaptivePathMaxLookaheadDistance = 24.0;
     constexpr double kAdaptiveErrorLookaheadCoefficient = 0.01;
-    constexpr double kMaxVelocityMetersPerSecond = 1; // from da poofs (calculate this for our robot)
+    constexpr double kMaxVelocityMetersPerSecond = 3; // from da poofs (calculate this for our robot)
 } // namespace ck
